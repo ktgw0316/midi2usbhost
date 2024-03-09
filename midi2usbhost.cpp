82a19e68dc2f6f8d94d805be2526997b45512084
Masahiro Kitagawa <arctica0316@gmail.com>
2024-03-09 13:54:00 +0900

    Change pin assign

--- midi2usbhost.cpp
+++ midi2usbhost.cpp
@@ -34,7 +34,7 @@
 #include "tusb.h"
 #include "usb_midi_host.h"
 #include "encoder.hpp"
-#include "pico-ssd1306/textRenderer/TextRenderer.h"
+#include "textRenderer/TextRenderer.h"
 
 // On-board LED mapping. If no LED, set to NO_LED_GPIO
 constexpr uint NO_LED_GPIO = 255;
@@ -46,13 +46,13 @@ constexpr uint LED_GPIO = 25;
 // the changes in the CMakeLists.txt file or in your environment. Note
 // that if you use UART0, then serial port debug will not be enabled
 #ifndef MIDI_UART_NUM
-#define MIDI_UART_NUM 1
+#define MIDI_UART_NUM 0
 #endif
 #ifndef MIDI_UART_TX_GPIO
-#define MIDI_UART_TX_GPIO 4
+#define MIDI_UART_TX_GPIO 16
 #endif
 #ifndef MIDI_UART_RX_GPIO
-#define MIDI_UART_RX_GPIO 5
+#define MIDI_UART_RX_GPIO 17
 #endif
 
 static void *midi_uart_instance;
@@ -61,10 +61,10 @@ static uint8_t midi_dev_addr = 0;
 using namespace encoder;
 
 // Create an encoder on the 3 ADC pins, using PIO 0 and State Machine 0
-constexpr uint PIN_A = 26;  // The A channel pin
-constexpr uint PIN_B = 28;  // The B channel pin
-constexpr uint PIN_C = 27;  // The common pin
-Encoder enc(pio0, 0, {PIN_A, PIN_B}, PIN_C);
+constexpr uint PIN_A_GPIO = 28;  // The A channel pin
+constexpr uint PIN_C_GPIO = 27;  // The common pin
+constexpr uint PIN_B_GPIO = 26;  // The B channel pin
+Encoder enc(pio0, 0, {PIN_A_GPIO, PIN_B_GPIO}, PIN_C_GPIO);
 
 constexpr uint8_t GM_PROGRAM_NUMBER_SIZE = 128;
 uint8_t sound = 0;
@@ -90,21 +90,165 @@ static void blink_led(void)
 
 static void init_display(void)
 {
-    constexpr uint I2C_PORT = 0;
-    constexpr uint I2C_PIN_SDA = 4;
-    constexpr uint I2C_PIN_SCL = 5;
+    constexpr uint I2C_SDA_GPIO = 20;
+    constexpr uint I2C_SCL_GPIO = 21;
 
-    i2c_init(I2C_PORT, 1000000);
-    gpio_set_function(I2C_PIN_SDA, GPIO_FUNC_I2C);
-    gpio_set_function(I2C_PIN_SCL, GPIO_FUNC_I2C);
-    gpio_pull_up(I2C_PIN_SDA);
-    gpio_pull_up(I2C_PIN_SCL);
+    i2c_init(i2c0, 1000000);
+    gpio_set_function(I2C_SDA_GPIO, GPIO_FUNC_I2C);
+    gpio_set_function(I2C_SCL_GPIO, GPIO_FUNC_I2C);
+    gpio_pull_up(I2C_SDA_GPIO);
+    gpio_pull_up(I2C_SCL_GPIO);
+    sleep_ms(250); // wait for display to power up
 }
 
-static void sound_name(char *buf, const uint8_t sound)
+const char sound_names[128][33] = {
+    /*   1 */ "Acoustic Piano",
+    /*   2 */ "Bright Piano",
+    /*   3 */ "Elec.Grand Piano"
+    /*   4 */ "Honky-tonk Piano",
+    /*   5 */ "Electric Piano 1",
+    /*   6 */ "Electric Piano 2",
+    /*   7 */ "Harpsichord",
+    /*   8 */ "Clavinet",
+    /*   9 */ "Celesta",
+    /*  10 */ "Glockenspiel",
+    /*  11 */ "Music Box",
+    /*  12 */ "Vibraphone",
+    /*  13 */ "Marimba",
+    /*  14 */ "Xylophone",
+    /*  15 */ "Tubular Bells",
+    /*  16 */ "Dulcimer",
+    /*  17 */ "Drawbar Organ",
+    /*  18 */ "Percussive Organ",
+    /*  19 */ "Rock Organ",
+    /*  20 */ "Church Organ",
+    /*  21 */ "Reed Organ",
+    /*  22 */ "Accordion",
+    /*  23 */ "Harmonica",
+    /*  24 */ "Tango Accordion",
+    /*  25 */ "Acou.Guitar(nylon)",
+    /*  26 */ "Acou.Guitar(steel)",
+    /*  27 */ "Elec.Guitar(jazz)",
+    /*  28 */ "Elec.Guitar(clean)",
+    /*  29 */ "Elec.Guitar(muted)",
+    /*  30 */ "Overdriv. Guitar",
+    /*  31 */ "Dist. Guitar",
+    /*  32 */ "Guitar Harmonics",
+    /*  33 */ "Acoustic Bass",
+    /*  34 */ "Elec.Bass(finger)",
+    /*  35 */ "Elec.Bass(pick)",
+    /*  36 */ "Fretless Bass",
+    /*  37 */ "Slap Bass 1",
+    /*  38 */ "Slap Bass 2",
+    /*  39 */ "Synth Bass 1",
+    /*  40 */ "Synth Bass 2",
+    /*  41 */ "Violin",
+    /*  42 */ "Viola",
+    /*  43 */ "Cello",
+    /*  44 */ "Contrabass",
+    /*  45 */ "Tremolo Strings",
+    /*  46 */ "Pizzicato Strings",
+    /*  47 */ "Orchestral Harp",
+    /*  48 */ "Timpani",
+    /*  49 */ "Str. Ensemble 1",
+    /*  50 */ "Str. Ensemble 2",
+    /*  51 */ "Synth Strings 1",
+    /*  52 */ "Synth Strings 2",
+    /*  53 */ "Choir Aahs",
+    /*  54 */ "Voice Oohs",
+    /*  55 */ "Synth Choir",
+    /*  56 */ "Orchestra Hit",
+    /*  57 */ "Trumpet",
+    /*  58 */ "Trombone",
+    /*  59 */ "Tuba",
+    /*  60 */ "Muted Trumpet",
+    /*  61 */ "French Horn",
+    /*  62 */ "Brass Section",
+    /*  63 */ "Synth Brass 1",
+    /*  64 */ "Synth Brass 2",
+    /*  65 */ "Soprano Sax",
+    /*  66 */ "Alto Sax",
+    /*  67 */ "Tenor Sax",
+    /*  68 */ "Baritone Sax",
+    /*  69 */ "Oboe",
+    /*  70 */ "English Horn",
+    /*  71 */ "Bassoon",
+    /*  72 */ "Clarinet",
+    /*  73 */ "Piccolo",
+    /*  74 */ "Flute",
+    /*  75 */ "Recorder",
+    /*  76 */ "Pan Flute",
+    /*  77 */ "Blown Bottle",
+    /*  78 */ "Shakuhachi",
+    /*  79 */ "Whistle",
+    /*  80 */ "Ocarina",
+    /*  81 */ "Lead 1 (square)",
+    /*  82 */ "Lead 2 (sawtooth)",
+    /*  83 */ "Lead 3 (calliope)",
+    /*  84 */ "Lead 4 (chiff)",
+    /*  85 */ "Lead 5 (charang)",
+    /*  86 */ "Lead 6 (voice)",
+    /*  87 */ "Lead 7 (fifths)",
+    /*  88 */ "Lead 8 (bass+lead)",
+    /*  89 */ "Pad 1 (new age)",
+    /*  90 */ "Pad 2 (warm)",
+    /*  91 */ "Pad 3 (polysynth)",
+    /*  92 */ "Pad 4 (choir)",
+    /*  93 */ "Pad 5 (bowed)",
+    /*  94 */ "Pad 6 (metallic)",
+    /*  95 */ "Pad 7 (halo)",
+    /*  96 */ "Pad 8 (sweep)",
+    /*  97 */ "FX 1 (rain)",
+    /*  98 */ "FX 2 (soundtrack)",
+    /*  99 */ "FX 3 (crystal)",
+    /* 100 */ "FX 4 (atmosphere)",
+    /* 101 */ "FX 5 (brightness)",
+    /* 102 */ "FX 6 (goblins)",
+    /* 103 */ "FX 7 (echoes)",
+    /* 104 */ "FX 8 (sci-fi)",
+    /* 105 */ "Sitar",
+    /* 106 */ "Banjo",
+    /* 107 */ "Shamisen",
+    /* 108 */ "Koto",
+    /* 109 */ "Kalimba",
+    /* 110 */ "Bagpipe",
+    /* 111 */ "Fiddle",
+    /* 112 */ "Shanai",
+    /* 113 */ "Tinkle Bell",
+    /* 114 */ "Agogo",
+    /* 115 */ "Steel Drums",
+    /* 116 */ "Woodblock",
+    /* 117 */ "Taiko Drum",
+    /* 118 */ "Melodic Tom",
+    /* 119 */ "Synth Drum",
+    /* 120 */ "Reverse Cymbal",
+    /* 121 */ "Gtr.Fret Noise",
+    /* 122 */ "Breath Noise",
+    /* 123 */ "Seashore",
+    /* 124 */ "Bird Tweet",
+    /* 125 */ "Telephone Ring",
+    /* 126 */ "Helicopter",
+    /* 127 */ "Applause",
+    /* 128 */ "Gunshot",
+};
+
+static void display_sound_name(pico_ssd1306::SSD1306 * pDisplay, uint8_t sound)
 {
-    // TODO
-    sprintf(buf, sizeof(buf), "Sound = %d", sound + 1);
+    using namespace pico_ssd1306;
+
+    pDisplay->clear();
+
+    // Top line is the General MIDI sound number
+    char sound_num_str[4];
+    snprintf(sound_num_str, sizeof(sound_num_str), "%d", sound + 1);
+    drawText(pDisplay, font_12x16, sound_num_str, 0, 0);
+
+    // Bottom line is the sound name
+    const char *sound_name = sound_names[sound];
+    drawText(pDisplay, font_8x8, sound_name, 0, 24);
+    pDisplay->sendBuffer();
+
+    printf("Sound = %d: %s\n", sound + 1, sound_name);
 }
 
 static void poll_midi_uart_rx(bool connected)
@@ -134,6 +278,10 @@ static void write_midi_uart_program_change(const uint8_t sound)
     }
 }
 
+inline u_int32_t positive_modulo(int32_t dividend, uint32_t divisor) {
+    return (dividend % divisor + divisor) % divisor;
+}
+
 int main() {
     bi_decl(bi_program_description("Provide a USB host interface for Serial Port MIDI."));
     bi_decl(bi_1pin_with_name(LED_GPIO, "On-board LED"));
@@ -145,6 +293,7 @@ int main() {
     sleep_ms(8000);
 
     board_init();
+    blink_led();
     printf("Pico MIDI Host to MIDI UART Adapter\r\n");
     tusb_init();
 
@@ -156,25 +305,20 @@ int main() {
     // enc.direction(REVERSED_DIR);
 
     enc.init();
-    printf("Sound = %d\n", sound + 1);
 
     // Create a new display object
     init_display();
-    pico_ssd1306::SSD1306 display = pico_ssd1306::SSD1306(I2C_PORT, 0x3D, pico_ssd1306::Size::W128xH64);
+    // pico_ssd1306::SSD1306 display(i2c0, 0x3D, pico_ssd1306::Size::W128xH64);
+    pico_ssd1306::SSD1306 display(i2c0, 0x3C, pico_ssd1306::Size::W128xH32);
+    display_sound_name(&display, sound);
 
     while (1) {
         tuh_task();
-        blink_led();
 
-        if (enc.delta() != 0)
-        {
-            sound = enc.count() % GM_PROGRAM_NUMBER_SIZE;
+        if (enc.delta() != 0) {
+            sound = positive_modulo(enc.count(), GM_PROGRAM_NUMBER_SIZE);
             write_midi_uart_program_change(sound);
-            printf("Sound = %d\n", sound + 1);
-
-            char *str[12]; // 128/12 = 10.6, so 12 chars is enough
-            sound_name(str, sound);
-            drawText(&display, font_12x16, str, 0, 0);
+            display_sound_name(&display, sound);
         }
 
         const bool connected = midi_dev_addr != 0 && tuh_midi_configured(midi_dev_addr);
